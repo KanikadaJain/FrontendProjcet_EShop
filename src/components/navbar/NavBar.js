17a68f634bc5616069202536e7b8ae59f7d4bec7
KanikadaJain <kanikadajain@gmail.com>
2025-01-20 00:43:27 +0530

    Navigation bar

--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -4,9 +4,10 @@ import React from 'react';
 import { AppBar, Toolbar, Typography, Button, InputBase } from '@mui/material';
 import SearchIcon from '@mui/icons-material/Search'; // Import search icon
 import { Link,useNavigate } from 'react-router-dom';
-import Container from "@mui/material/Container";
-import {ShoppingCart} from "@mui/icons-material";
+import './NavBar.css'; // Import the CSS file
 import Box from "@mui/material/Box";
+import {ShoppingCart} from "@mui/icons-material";
+
 const NavBar = ({ isLoggedIn, isAdmin, onLogout, onSearch }) => {
   const navigate = useNavigate(); 
   const handleLogout = () => {
@@ -26,51 +27,58 @@ const NavBar = ({ isLoggedIn, isAdmin, onLogout, onSearch }) => {
 
 console.log("nav"+isAdmin);
   return (
-    <AppBar sx={{ bgcolor: "#3f51b5", position: 'fixed' }}>
-      <Container maxWidth={false}>
-      <Toolbar disableGutters>
-        <ShoppingCart sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
-          <Typography variant="h6" noWrap component="a" href="/home"
-						sx={{mr: 2, display: { xs: 'none', md: 'flex' },fontFamily: 'monospace',
-						color: 'inherit', textDecoration: 'none',}}>upGrad E-Shop
-          </Typography>
+    <AppBar sx={{ bgcolor: "#3f51b5" }}>
+      <Toolbar>
+      <ShoppingCart sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
+			<Typography variant="h6" noWrap component="a" 
+						sx={{ mr: 2, display: { xs: 'none', md: 'flex' }, fontFamily: 'monospace',
+            color: 'inherit', textDecoration: 'none',}}
+			>upGrad E-Shop
+			</Typography>
 
-          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-
-
-
-          </Box>
-					<ShoppingCart sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
-					<Typography variant="h6" noWrap component="a" href=""
-						sx={{ mr: 2, display: { xs: 'flex', md: 'none' },flexGrow: 1,
-            fontFamily: 'monospace', color: 'inherit', textDecoration: 'none',}}
-					>upGrad E-Shop</Typography>
-          <Box sx={{ flexGrow: 1 }} />          
+        {/* Updated Search Input */}
+       
           
-          {isLoggedIn ? (
-            <>
-              {isAdmin && (
-                <Link to="/add-products" className="navLink">
-                  <Button color="inherit">Add Products</Button>
-                </Link>
-              )}
-            {/*} <Button className="logoutButton" onClick={onLogout}>Logout</Button>*/}
-              <Button className="logoutButton" style={{width:'40px'}} onClick={handleLogout}>Logout</Button>
-            </>
-          ) : (
-            <>
-              <Link to="/login" className="navLink">
-                <Button sx={{ my: 2, color: 'white', display: 'block', textTransform: "none" }}
-								>Login</Button>
+        
+        {isLoggedIn ? (
+          <>
+          <Box sx={{ flexGrow: 1 }} />
+          <div className="searchBox">
+            <SearchIcon className="searchIcon" />
+            <InputBase
+              placeholder="Search…"
+              inputProps={{ 'aria-label': 'search' }}
+              onKeyPress={handleSearch}
+              className="searchInput"
+            />
+          </div>
+            {isAdmin && (
+              <Link to="/add-products" className="navLink">
+                <Button color="inherit">Add Products</Button>
+              </Link>
+            )}
+           {/*} <Button className="logoutButton" onClick={onLogout}>Logout</Button>*/}
+            <Button className="logoutButton" style={{width:'40px'}} onClick={handleLogout}>Logout</Button>
+            <Link to="/" className="navLink"><Button color="inherit">Home</Button></Link>
+          </>
+        ) : (
+          <>
+          <Box sx={{ flexGrow: 1 }} />
+          <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
+            <Link to="/login" className="navLink">
+                <Button sx={{ my: 2, color: 'white', display: 'block', textTransform: "none" }}>
+                  <u>Login</u>
+                </Button>
               </Link>
               <Link to="/signup" className="navLink">
-              <Button sx={{ my: 2, color: 'white', display: 'block', textTransform: "none" }}
-								>Sign Up</Button>
-              </Link>
-            </>
-          )}
-        </Toolbar>
-      </Container>
+                <Button sx={{ my: 2, color: 'white', display: 'block', textTransform: "none" }}>
+                  <u>Sign Up</u>
+                </Button>
+            </Link>
+          </Box>
+          </>
+        )}
+      </Toolbar>
     </AppBar>
   );
 };
